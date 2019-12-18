30fe5f73c56f8bac834be0a148d3391221c5d65b
Tal Sagi <Tal@Eusers-MacBook-Pro.local>
2019-12-18 03:36:59 -0500

    arrays of 3 languages counting to ten that are converted to a hash

---
+++ arrays_to_hash.rb
@@ -0,0 +1,26 @@
+
+digits = ['1','2','3','4','5','6','7','8','9']
+en = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+fr = ['un','deux','trois','quatre','cinq','six','sept','huit','neuf']
+hb = ['echad', 'shtaym', 'shalosh', 'arba', 'chamesh', 'shesh', 'sheva', 'shmona', 'teisha', 'eser']
+# Write the necessary code to transform these arrays into a hash with the 
+# following format:
+
+# {
+#   1: {french: 'un', english: 'one'},
+#   2: {french: 'deux', english: 'two'},
+#   3: {french: 'trois', english: 'three'},
+#   ...
+#   9: {french: 'neuf', english: 'nine'}
+# }
+
+output = {}
+(0...digits.length).each {|i|
+   output["#{digits[i]}:"] = {
+                    "french:" => fr[i],
+                    "english:" => en[i],
+                    "hebrew:" => hb[i]
+                    }
+}
+print output
+puts
